0a502a7429729cbf99672039052277f10f9128e9
Alexey <leha.kurdyukov.99@mail.ru>
2022-10-28 12:30:13 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+	return(
+		<header className="header">
+			<div className="header__wrapper">		
+				<div className="header__logo-container">
+					<div className="header__logo"></div>
+					<span className="header__title"></span>
+				</div>
+				
+			</div>
+		</header>
+	)
+}
+
+
+function App() {
+	return(
+	<div className="App"></div>
+	)
+}
+
+export default App;
